d8bc0e3eed0c569667d74c7344060da6ceef2e75
muhammadabrar <42229174+muhammadabrar@users.noreply.github.com>
2022-12-26 10:00:32 +0500

    start

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <Head>
+        <title>Muhammad Abrar | Portfolio</title>
+        <meta name="description" content="Muhammad Abrar Portfolio website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
